b2ea00f3d0d9f70386942eb46f24398d2456d022
Vitaliy Zakharov <zak125@mail.ru>
2025-11-09 21:13:35 +0300

    Практика нагрузочного тестирования. Часть 3

--- scenarios/http/gateway/new_user_issue_physical_card/scenario.py
+++ scenarios/http/gateway/new_user_issue_physical_card/scenario.py
@@ -34,5 +34,5 @@ class IssuePhysicalCardSequentialTaskSet(GatewayHTTPSequentialTaskSet):
         )
 
 
-class GetAccountsUser(LocustBaseUser):
+class IssuePhysicalCardsUser(LocustBaseUser):
     tasks = [IssuePhysicalCardSequentialTaskSet]
